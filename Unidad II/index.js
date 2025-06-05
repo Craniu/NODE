1e26df480acc80735efebcf94b79b6ad1303f415
Cristoffer Venega DLC <cristoffer.venega@gmail.com>
2025-06-04 22:55:36 -0400

    nose q hago

--- Unidad II/index.js
+++ Unidad II/index.js
@@ -1,6 +1,34 @@
 import express from 'express'
+import 'dotenv/config'
+import fs from 'fs'
+
 
 const app = express()
-const port = 300
+const port = process.env.PORT ?? 3000
 
-app.listen(port, console.log(´${PORT}´))
+
+app.use(express.json())
+
+app.get('/home',(req, res) =>{
+    res.send('Hola Gente')
+})
+
+app.get('/heroes',(req, res) => {
+    const heroes = JSON.parse(fs.readFileSync('data/heroes.json', 'utf8'))
+    res.json(heroes)
+})
+
+app.post('/heroes', (req, res) =>{
+    try {
+            const heroe = req.body
+            const heroes = JSON.parse(fs.readFileSync('data/heroes.json', 'utf8'))
+            heroes.push(heroe)
+            fs.writeFileSync('data/heroes.json', JSON.stringify(heroes))
+            res.send('Se creo el heroe')
+        }catch (error){
+            console.error('[error] => ',error)
+        }
+})
+
+
+app.listen(port, console.log(`🔥Server : http://localhost:${port} is On`))
